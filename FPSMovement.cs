f72830dab372245438502a2452e5b41380cab260
truespirit7 <57035794+truespirit7@users.noreply.github.com>
2020-09-26 00:01:56 +0700

    Add files via upload

---
+++ FPSMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMovement : MonoBehaviour
+{
+    public float gravity = -10;
+    public float moveSpeed = 6;
+    private CharacterController _characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        if (_characterController == null) {
+            Debug.Log("characterController is null"); 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * moveSpeed;
+        float deltaZ = Input.GetAxis("Vertical") * moveSpeed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, moveSpeed);
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _characterController.Move(movement);
+    }
+}
---
+++ MouseLook.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // выпадающий список для настройки осей вращения
+    public enum RotationAxes
+    {
+        
+        X,
+        Y,
+        XandY
+    }
+    public RotationAxes _axes = RotationAxes.XandY;
+    // чувствительность мыши
+    public float _rotationSpeedHor = 5.0f;
+    public float _rotationSpeedVer = 5.0f;
+    // максимальный угол вращения по вертикали
+    public float MaxVert = 45;
+    public float MinVert = -45;
+    // текущий угол вращения
+    private float _rotationX = 0;
+    private void Start()
+    {
+      Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+        {
+            body.freezeRotation = true;
+        }
+    }
+    private void Update()
+    {
+        if (_axes == RotationAxes.XandY) {
+            _rotationX -= Input.GetAxis("Mouse Y") * _rotationSpeedVer;
+            _rotationX = Mathf.Clamp(_rotationX, MinVert, MaxVert);
+            float delta = Input.GetAxis("Mouse X") * _rotationSpeedVer;
+            float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        else if (_axes == RotationAxes.X) {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * _rotationSpeedHor, 0);
+        }
+        else if (_axes == RotationAxes.Y) {
+
+            // вычитаем значение из угла вращения
+            _rotationX -= Input.GetAxis("Mouse Y") * _rotationSpeedVer;
+            // ограничиваем угол вращения
+            _rotationX = Mathf.Clamp(_rotationX, MinVert, MaxVert);
+            float _rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
+        }
+
+    }
+}
